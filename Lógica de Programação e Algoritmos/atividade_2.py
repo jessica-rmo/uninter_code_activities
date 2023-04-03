094783db93abc94f541af298013f4295a26e359a
jessica-rmo <jrmo@cin.ufpe.br>
2023-04-02 21:02:55 -0300

    q2

--- Lógica de Programação e Algoritmos/atividade_2.py
+++ Lógica de Programação e Algoritmos/atividade_2.py
@@ -1,50 +1,51 @@
 print("""
->>> Bem-vindo(a) ao calculador de descontos! <<<
->> Desenvolvido por Jéssica Raquel de Melo Oliveira (RU: 3593002) <<
+::::::::: +  Bem-vindo(a) à lanchonete da Jéssica  + :::::::::
+Desenvolvido por Jéssica Raquel de Melo Oliveira (RU: 3593002)
+
+.---------------------------------------------.
+| ::::::::: +   C A R D Á P I O   + ::::::::: |
++---------+-----------------------+-----------+
+|  Código |       Descrição       |   Valor   |
++---------+-----------------------+-----------+
+|  100    | Cachorro-Quente       | R$   9.00 |       
+|  101    | Cachorro-Quente Duplo | R$  11.00 |       
+|  102    | X-Egg                 | R$  12.00 |       
+|  103    | X-Salada              | R$  13.00 |       
+|  104    | X-Bacon               | R$  14.00 |       
+|  105    | X-Tudo                | R$  17.00 |       
+|  200    | Refrigerante Lata     | R$   5.00 |       
+|  201    | Chá Gelado            | R$   4.00 |       
++---------+-----------------------+-----------+
 """)
 
-# ---- variaveis iniciais ---- #
-preco_unidade = 0.0
-qtd_produto = 0
-desconto = 0.0
+cardapio = {  # dicionário que contém as informações do cardápio
+    100: {'descricao': 'Cachorro-Quente', 'valor': 9.0},
+    101: {'descricao': 'Cachorro-Quente Duplo', 'valor': 11.0},
+    102: {'descricao': 'X-Egg', 'valor': 12.0},
+    103: {'descricao': 'X-Salada', 'valor': 13.0},
+    104: {'descricao': 'X-Bacon', 'valor': 14.0},
+    105: {'descricao': 'X-Tudo', 'valor': 17.0},
+    200: {'descricao': 'Refrigerante Lata', 'valor': 5.0},
+    201: {'descricao': 'Chá Gelado', 'valor': 4.0}
+}
+
+# ----- variavais iniciais ----- #
+total_pedido = 0.0
 
 while True:
-    try:  # verificação da entrada para o preço da unidade
-        preco_unidade = float(input("Informe o valor de 1 unidade do produto: "))
-        if (preco_unidade <= 0):
-            raise ValueError
+    codigo = int(input("Informe o código do produto desejado: "))
+
+    if codigo not in cardapio:  # verifica se o código informado existe no cardápio
+        print("Opção inválida.")
+        continue
+
+    total_pedido += cardapio[codigo]['valor']
+    print(f"Você pediu um {cardapio[codigo]['descricao']} no valor de R$ {cardapio[codigo]['valor']:.2f}")
+
+    opcao = int(input("Deseja pedir mais alguma coisa?\n 1 - Sim\n 0 - Não\n>> "))
+    if opcao == 0:
         break
-    except ValueError:
-        print("(!) Insira um valor numérico válido maior que zero.")
-        continue  # retorna para o while da linha 6
+    elif opcao == 1:
+        continue
 
-while True:
-    try:  # verificação da entrada para a quantidade do produto
-        qtd_produto = int(input("Informe a quantidade do produto desejada: "))
-        if (qtd_produto <= 0):
-            raise ValueError
-        break
-    except ValueError:
-        print("(!) Insira um valor inteiro e maior que zero.")
-        continue  # retorna para o while da linha 16
-
-valor_liquido = preco_unidade * qtd_produto
-print(">>> O valor total sem desconto é: R$ {:.2f}".format(valor_liquido))
-
-# loop para calcular o descondo de acordo com a entrada do usuario
-if qtd_produto < 10:
-    desconto = 0
-elif qtd_produto < 100:
-    desconto = 0.05
-elif qtd_produto < 1000:
-    desconto = 0.1
-else:
-    desconto = 0.15
-
-valor_com_desconto = valor_liquido * (1 - desconto)
-if (desconto == 0):  # if para printar mensagem caso não haja desconto
-    print("    Nenhum desconto foi aplicado.")
-else:  # caso haja desconto, informa o valor calculado ao usuário
-    porcentagem = int(desconto * 100)  # calcula o valor da porcentagem para exibir ao usuario
-    print(">>> O valor total com desconto é: R$ {a:.2f} (desconto: {b:}%)".format(a=valor_com_desconto,
-    b=porcentagem))
+print(f"O valor total a ser pago pelo pedido é: R$ {total_pedido:.2f}")
